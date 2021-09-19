58eff027730889fd57af5b55e146ec99f9fbb35d
Jeff Wesner <jeff.wesner@usd.edu>
2021-09-19 15:07:53 -0500

    add models

--- applied_bayesian_modeling/prior_predictive.R
+++ applied_bayesian_modeling/prior_predictive.R
@@ -18,16 +18,16 @@ d <- as_tibble(mtcars)
 # b ~ ?
 # sigma ~ ?
 
-N = 1  # number of simulations (change as needed)
+N = 100  # number of simulations (change as needed)
 
 # simulate priors
-priors <- tibble(a = rnorm(N, 0, 10),
-                 b = rnorm(N, 0, 10),
-                 sigma = rexp(N, 0.0001),
+priors <- tibble(a = rnorm(N, 25, 5),
+                 b = rnorm(N, 0, 0.1),
+                 sigma = rexp(N, 1),
                  sim = 1:N)
 
 # data (only the x values, since we're simulating y and mu and pretending we don't have them yet)
-x <- d$hp
+x <- d$hp - mean(d$hp)
 
 # combine and simulate
 prior_and_x <- priors %>% expand_grid(x = x) %>%    # combine priors and x's
@@ -42,9 +42,30 @@ prior_and_x %>%
   labs(y = "sim")
 
 
+# fit with rethinking
+gaus_ret <- ulam(
+  alist(
+    mpg ~ dnorm(mu, sigma),
+    mu <- a + b*hp_c,
+    a ~ dnorm(25, 5),
+    b ~ dnorm(0, 0.1),
+    sigma ~ dexp(1)),
+  data = list(mpg = d$mpg, hp_c = d$hp - mean(d$hp)),
+  cores = 4)
+
+
+# fit with brms
+gaus_brm <- brm(mpg ~ hp_c, 
+                data = d %>% mutate(hp_c = hp - mean(hp)),
+                family = gaussian(),
+                prior = c(prior(normal(25, 5), class = "Intercept"),
+                          prior(normal(0, 0.1), class = "b"),
+                          prior(exponential(1), class = "sigma")), 
+                cores = 4)
+
 # gamma regression  -------
 
-# y ~ dgamma(scale, shape)
+# y ~ dgamma2(scale, shape)
 # scale = mu/shape
 # log(mu)  = a + bx
 # a ~ ?
@@ -56,7 +77,8 @@ N = 10  # number of simulations
 # simulate priors
 priors <- tibble(a = rnorm(N, 2, 1),
                  b = rnorm(N, 0, 1),
-                 shape = rexp(N, 1),
+                 shape = rexp(N, 2), # for brms
+                 scale = rexp(N, 2), # for rethinking
                  sim = 1:N)
 
 # data (only the x values, since we're simulating y and mu and pretending we don't have them yet)
@@ -65,15 +87,34 @@ x <- (d$hp - mean(d$hp))/sd(d$hp)
 # combine and simulate
 prior_and_x <- priors %>% expand_grid(x = x) %>%    # combine priors and x's
   mutate(mu = exp(a + b*x),                         # simulate regressions
-         y = rgamma(nrow(.), scale = mu/shape, shape))  # simulate data (e.g., y_rep)
-          
-
+         y = rgamma(nrow(.), scale = mu/shape, shape),   # simulate data (e.g., y_rep) - brms parameterization
+         y_scale = rgamma(nrow(.), shape = mu/scale, scale))  # simulate data (e.g., y_rep) - rethinking parameterization
 # plot
 prior_and_x %>% 
   ggplot(aes(x = x, y = mu, group = sim)) + 
   geom_line() +
   geom_point(aes(y = y)) +
   labs(y = "sim") 
+
+# fit with rethinking (NOTE: rethinking and brms use different parameterizations for gamma)
+gamma_ret <- ulam(
+  alist(
+    mpg ~ dgamma2(mu, scale),
+    log(mu) <- a + b*hp_c,
+    a ~ dnorm(3, 1),
+    b ~ dnorm(0, 1),
+    scale ~ dexp(0.1)),
+  data = list(mpg = d$mpg, hp_c = (d$hp - mean(d$hp))/sd(d$hp)),
+  cores = 4)
+
+# fit with brms (NOTE: rethinking and brms use different parameterizations for gamma)
+gamma_brm <- brm(mpg ~ hp_c, 
+                 family = Gamma(link = "log"),
+                 data = d %>% mutate(hp_c = (hp - mean(hp))/sd(hp)),
+                 prior = c(prior(normal(3, 1), class = "Intercept"),
+                           prior(normal(0, 1), class = "b"),
+                           prior(exponential(0.1), class = "shape")),
+                 cores = 4)
 
 # gaussian anova  -------
 
